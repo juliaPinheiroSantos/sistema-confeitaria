52923d892bc7ed786cd05ab13d273ffb12759607
davijnunes0 <likerockstar254@gmail.com>
2026-02-01 13:06:08 -0300

    Corrijindo o teste das entidades no banco de dadoa

--- sistemaConfeitaria/src/app/Main.java
+++ sistemaConfeitaria/src/app/Main.java
@@ -10,27 +10,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             CreateTables.createTableUser();
-
-            char[] password = "julia123".toCharArray();
-            User user = new User(1, "Julia", "Pinheiro", "susulaju123@gmail.com", password); // 1 = id_person (person já deve existir)
-            
-            
-            RepositoryUser repositoryUser = new RepositoryUser();
-            boolean userCreated = repositoryUser.createUser(user);
-            
-            if (userCreated) {
-                System.out.println("Usuário criado com sucesso!");
-            } else {
-                System.out.println("Usuário já existe ou erro ao criar.");
-            }
-
-            boolean successful = EncryptionService.checkPassword("julia123".toCharArray(), user.getPasswordHash());
-
-            if (successful) {
-                System.out.println("Login realizado com sucesso! Bem-vindo.");
-            } else {
-                System.out.println("Senha incorreta! Acesso negado.");
-            }
+            System.out.println("Tabelas criadas com sucesso!");
         } catch (Exception e) {
             System.err.println("Erro: " + e.getMessage());
             e.printStackTrace();
